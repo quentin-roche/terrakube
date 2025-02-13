9d524c94c053fd1a3aa30fde7f1d9330cd645429
roche-quentin <roche.quentin@bcg.com>
2025-02-13 02:01:06 +0000

    force load NetworkProxyAutoConfiguration

--- api/src/main/java/org/terrakube/api/ServerApplication.java
+++ api/src/main/java/org/terrakube/api/ServerApplication.java
@@ -6,12 +6,15 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import com.orange.common.springboot.autoconfigure.proxy.NetworkProxyAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.terrakube.api.repository")
 @EnableAsync
 @EnableCaching
 @EnableScheduling
+@ImportAutoConfiguration(NetworkProxyAutoConfiguration.class)
 public class ServerApplication {
 
 	public static void main(String[] args) {
