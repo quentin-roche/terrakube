2de26f32e8135c2bdd02d91973f0bc5be3140a5d
roche-quentin <roche.quentin@bcg.com>
2025-02-17 16:14:31 +0100

    use json instead of form data

--- executor/src/main/java/org/terrakube/executor/plugin/tfoutput/local/LocalTerraformOutputImpl.java
+++ executor/src/main/java/org/terrakube/executor/plugin/tfoutput/local/LocalTerraformOutputImpl.java
@@ -6,8 +6,11 @@ import lombok.NonNull;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.terrakube.client.model.organization.job.TfOutputRequest;
+import org.terrakube.client.model.response.Response;
 import org.terrakube.executor.plugin.tfoutput.TerraformOutput;
 import org.terrakube.client.TerrakubeClient;
+
+import java.util.List;
 
 @Builder
 @Getter
@@ -26,6 +29,7 @@ public class LocalTerraformOutputImpl implements TerraformOutput {
 
         TfOutputRequest tfOutputRequest = new TfOutputRequest();
         tfOutputRequest.setData(output + outputError);
-        return terrakubeClient.uploadOutput(tfOutputRequest, organizationId, jobId, stepId).getData().getUrl();
+        Response<String> listOrganization = terrakubeClient.uploadOutput(tfOutputRequest, organizationId, jobId, stepId)
+        return listOrganization.getData();
     }
 }
