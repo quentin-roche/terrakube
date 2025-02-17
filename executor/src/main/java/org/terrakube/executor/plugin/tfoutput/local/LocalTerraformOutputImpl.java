e68a09a5101972221f2769e9bdc3f68ebdb273af
roche-quentin <roche.quentin@bcg.com>
2025-02-17 13:58:53 +0100

    use json instead of form data

--- executor/src/main/java/org/terrakube/executor/plugin/tfoutput/local/LocalTerraformOutputImpl.java
+++ executor/src/main/java/org/terrakube/executor/plugin/tfoutput/local/LocalTerraformOutputImpl.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.terrakube.client.model.organization.job.TfOutputRequest;
 import org.terrakube.executor.plugin.tfoutput.TerraformOutput;
 import org.terrakube.client.TerrakubeClient;
 
