a1a7832ef38e6da4fc4171cd4af4656e6594bdd5
roche-quentin <roche.quentin@bcg.com>
2025-03-04 09:22:41 +0000

    Reduce token expiry time for AWS Postgres

--- api/src/main/java/org/terrakube/api/plugin/datasource/PostgresAwsIamAuthDataSource.java
+++ api/src/main/java/org/terrakube/api/plugin/datasource/PostgresAwsIamAuthDataSource.java
@@ -18,7 +18,7 @@ public class PostgresAwsIamAuthDataSource extends PGSimpleDataSource {
     private Region awsRegion;
     private String authToken;
     private long lastTokenRefresh = 0;
-    private static final long TOKEN_EXPIRY_TIME = 10L * 60L * 1000L;
+    private static final long TOKEN_EXPIRY_TIME = 5L * 60L * 1000L;
 
     @Override
     public Connection getConnection(String user, String password) throws SQLException {
